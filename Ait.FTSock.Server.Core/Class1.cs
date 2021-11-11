df403ab8364db43ea78e5e5c38ccec2c8914d2d4
Arthur Stragier <71759234+ArthurStragier5@users.noreply.github.com>
2021-11-11 16:52:53 +0100

    Add IPv4Helper class

--- Ait.FTSock.Server.Core/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Ait.FTSock.Server.Core
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Ait.FTSock.Server.Core/Helpers/IPv4Helper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ait.FTSock.Server.Core.Helpers
+{
+    public class IPv4Helper
+    {
+    }
+}
